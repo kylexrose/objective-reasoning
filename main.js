26d8fb9f648802f1c8aee057018a19dbd565cf61
kylexrose@gmail.com <kylexrose@gmail.com>
2021-03-09 15:47:51 -0500

    finished all code and passed all tests

--- main.js
+++ main.js
@@ -1,9 +1,39 @@
 // ┌─────────────────┐
 // │ Your code here! │	
 // └─────────────────┘
+function isAdmin(obj){
+    return obj.userRole === "ADMIN"
+}
 
+function getEmail(obj){
+    return obj.firstName.toLowerCase() + '.' +
+    obj.lastName.toLowerCase() + "@codeimmersives.com";
+}
 
+function getPlaylistLength(obj){
+    return obj.songs.length;
+}
 
+function getHardestHomework(arr){
+    if (arr.length === 0){
+        return "";
+    }
+    let lowest = arr[0];
+    for (let i = 1; i < arr.length; i++){
+        if(arr[i].averageScore < lowest.averageScore){
+            lowest = arr[i]
+        }
+    }
+    return lowest.name;
+}
+
+function createPhonebook(names, phones){
+    let obj = {};
+    for (let i = 0; i < names.length; i++){
+        obj[names[i]] = phones[i];
+    }
+    return obj;
+}
 // ┌─────────────────────────────────────┐
 // │ Do not modify code below this line. │
 // └─────────────────────────────────────┘
